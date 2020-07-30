38ebef336fb1ce48e9fac65673221cd3d60b8a2c
Arunima A V <53604609+Arunima97@users.noreply.github.com>
2020-07-30 12:55:27 +0530

    Add files via upload
    error fix

---
+++ subcatalog.py
@@ -0,0 +1,5 @@
+The proposed system can be extended. Mobile application for shop owner and user 
+using Goggle map API. User can locate different shops and 
+roots through the map. Push notifications are given to user when new products are arrived. 
+Based on the locations and user interest offers can be given to through the mobile applications. Chat box was created for the communication between user and shop owner. Using AI pets can be identified. Payment gateways are invented. Images will be compressed to base64 conversion algorithms.
+
