833417cadd535af910c63d523362e5b88209a06c
Arunima A V <53604609+Arunima97@users.noreply.github.com>
2020-07-30 12:42:18 +0530

    Add files via upload
    error fix

---
+++ image.py
@@ -0,0 +1,7 @@
+System helps to create an advanced pet catalog. All products and pet details are 
+updated with photos and videos .Simple to manage multiple shops. Maintain a lifetime
+ relation with customer. Customer can request for any help and know about the entire 
+shop through customer module. Thus the main objective is to establish and develop an 
+online platform that will allow give an entire picture of the pet shop to customers 
+i.e., to   provide an online catalog to display the products and accessories available in 
+the pet shop. 
